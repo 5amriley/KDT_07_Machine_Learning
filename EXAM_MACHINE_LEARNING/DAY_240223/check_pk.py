10866e47d2fbd43889747a33fdbf167b85c1c744
5amriley <jamesbyun1998@gmail.com>
2024-02-27 09:24:50 +0900

    Initial commit

---
+++ EXAM_MACHINE_LEARNING/DAY_240223/check_pk.py
@@ -0,0 +1,16 @@
+## 설치 패키지 버전 체크
+import pandas as pd
+import numpy as np
+import sklearn
+import matplotlib
+import seaborn
+import bs4
+import selenium
+import koreanize_matplotlib
+
+print(f'Pandas version: {pd.__version__}')
+print(f'Scikit-learn version: {sklearn.__version__}')
+print(f'Matplotlib version: {matplotlib.__version__}')
+print(f'Seaborn version: {seaborn.__version__}')
+print(f'beautifulsoup version: {bs4.__version__}')
+print(f'selenium version: {selenium.__version__}')
